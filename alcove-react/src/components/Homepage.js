246af681f907e107bb196516ef0f9808fdc215f0
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-07-29 18:27:03 +0200

    navbar

--- alcove-react/src/components/Homepage.js
+++ alcove-react/src/components/Homepage.js
@@ -9,9 +9,17 @@ class Homepage extends React.Component {
     return (
       <>
         <h1>L'Alcôve</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra id turpis ac rutrum. Interdum et malesuada fames ac ante ipsum primis in faucibus</p>
-        <img src="https://via.placeholder.com/375x250" alt="home-pic" />   
+        <p>Lorem ipsum dolor sit amet,
+        consectetur adipiscing 
+        elit. Curabitur viverra 
+        id turpis ac rutrum.
+         Interdum et malesuada fames
+         ac ante ipsum primis 
+         in faucibus</p>
+        <img src="https://via.placeholder.com/375x250" alt="home-pic" />
+        <div id='store'>
         <Product user={this.props.user} cart={this.props.cart} updateCart={this.props.updateCart}/>
+        </div>
       </>
     )
   }
--- alcove-react/src/components/Navbar.js
+++ alcove-react/src/components/Navbar.js
@@ -1,39 +1,63 @@
-import React from 'react';
-import burger from '../images/menu_burger.png'
-import icon from '../images/logo_alcove.svg'
-import avatar from '../images/profil.png'
-import cart from '../images/shopping-bag.svg'
-import { Link } from 'react-router-dom';
-import MediaQuery from 'react-responsive'
-import './Navbar.css';
+import React from "react";
+import burger from "../images/menu_burger.png";
+import icon from "../images/logo_alcove.svg";
+import avatar from "../images/profil.png";
+import cart from "../images/shopping-bag.svg";
+// import { Link } from "react-router-dom";
+import MediaQuery from "react-responsive";
+import { HashLink as Link } from "react-router-hash-link";
+import "./Navbar.css";
 
 class Navbar extends React.Component {
   render() {
     return (
       <nav className="navbar">
-      
-      <MediaQuery maxWidth={1224}>
-      <img className="burger" src={burger} alt="burger" />
-      </MediaQuery>
+        <MediaQuery maxWidth={1224}>
+          <img className="burger" src={burger} alt="burger" />
+        </MediaQuery>
 
         <Link to="/">
           <img src={icon} className="icon" alt="alcoveIcon" />
         </Link>
-        <div className="navbar-container">
-          {this.props.user ? 
+
+        <ul className="navbar-container">
+          <MediaQuery minWidth={1225}>
+            <li>
+              <Link
+                to={{ pathname: "https://www.planity.com/" }}
+                target="_blank"
+              >
+                <p>Prendre rendez-vous</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/services"><p>Nos tarifs</p></Link>
+            </li>
+            <li>
+              <Link to="/#store"><p>La boutique</p></Link>
+            </li>
+          </MediaQuery>
+          <li>
+          {this.props.user ? (
             <Link to="/profile">
               <img className="avatar" src={avatar} alt="avatar" />
-            </Link> : 
+            </Link>
+          ) : (
             <Link to="/signup">
               <img className="avatar" src={avatar} alt="avatar" />
-            </Link>}
+            </Link>
+          )}
+          </li>
+          <li>
           <Link to="/cart">
             <img className="cart" src={cart} alt="cart" />
           </Link>
+          </li>
+          
           {/* {this.props.user ? <Link to="/cart"><img src={cart} alt="cart" /></Link> : <Link to="/signup"><img src={cart} alt="cart" /></Link>} */}
-        </div>
+        </ul>
       </nav>
-    )
+    );
   }
 }
 
