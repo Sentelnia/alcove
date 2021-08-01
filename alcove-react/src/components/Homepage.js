0980d1cd77676be230d6a50fc8b48b5940d423ef
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-08-01 23:35:05 +0200

    homepage

--- alcove-react/src/components/Homepage.js
+++ alcove-react/src/components/Homepage.js
@@ -1,26 +1,21 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import Product from './products/Product';
+import institut from '../images/institut.jpeg'
 import './Homepage.css';
 
 class Homepage extends React.Component {
   render() {
     console.log('props HP',this.props)
     return (
-      <>
+      <div className='homepage'>
         <h1>L'Alcôve</h1>
-        <p>Lorem ipsum dolor sit amet,
-        consectetur adipiscing 
-        elit. Curabitur viverra 
-        id turpis ac rutrum.
-         Interdum et malesuada fames
-         ac ante ipsum primis 
-         in faucibus</p>
-        
+        <p>L’Alcôve est un espace dédié à la beauté de votre regard et de vos ongles, un véritable cocon où l'esthétique côtoie la sérénité. </p>
+        <img className='imgIntro' src={institut} alt='institut'/>
         <div id='store'>
         <Product user={this.props.user} cart={this.props.cart} updateCart={this.props.updateCart}/>
         </div>
-      </>
+      </div>
     )
   }
 }
