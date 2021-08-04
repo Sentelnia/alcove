b8332144ad3b8c9fabb1ff9ffa394c62e0e53c5d
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-08-04 19:20:20 +0200

    profil

--- alcove-react/src/components/auth/Profile.js
+++ alcove-react/src/components/auth/Profile.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
+import React from "react";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
-import authService from './auth-service.js';
-import { Redirect } from 'react-router-dom';
-import Order from '../orders/Order.js';
-
+import authService from "./auth-service.js";
+import { Redirect } from "react-router-dom";
+import Order from "../orders/Order.js";
+import "./Profil.css";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 class Profile extends React.Component {
@@ -21,11 +21,11 @@ class Profile extends React.Component {
     city: this.props.user.city || "",
     userNumber: this.props.user.userNumber || "",
 
-    currentPassword: '',
-    newPassword: '',
+    currentPassword: "",
+    newPassword: "",
 
-    msg: ''
-  }
+    msg: "",
+  };
 
   componentDidUpdate(prevProps, prevState) {
     // executée qd n'importe quelle props + nimporte quel state change !!
@@ -42,7 +42,7 @@ class Profile extends React.Component {
         supp: this.props.user.supp || "",
         zip: this.props.user.zip || "",
         city: this.props.user.city || "",
-      })
+      });
     }
   }
 
@@ -50,67 +50,74 @@ class Profile extends React.Component {
     let regEx = /^[0-9]*$/; //autorise chiffre de 0 à 9 + RAZ
     const { name, value } = event.target;
 
-    if (name !== 'zip') {
+    if (name !== "zip") {
       this.setState({ [name]: value });
       return;
     }
 
-    if ((name === 'zip') && (regEx.test(event.target.value) && value.length < 6)) {
+    if (name === "zip" && regEx.test(event.target.value) && value.length < 6) {
       this.setState({ [name]: value });
       return;
     }
-  }
+  };
 
   logout = (event) => {
-    authService.logout()
-      .then(() => {
-        this.props.updateUser(false);
-      });
-  }
+    authService.logout().then(() => {
+      this.props.updateUser(false);
+    });
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
 
     /////////////////EDIT/////////////
-    authService.edit(this.state)
-      .then(response => {
+    authService
+      .edit(this.state)
+      .then((response) => {
         this.setState({ msg: "" });
-        this.props.updateUser(this.state)
+        this.props.updateUser(this.state);
       })
-      .catch(err => this.setState({ msg: err.response.data.message }))
-  }
+      .catch((err) => this.setState({ msg: err.response.data.message }));
+  };
 
   handleSubmitUpdatePassword = (event) => {
     event.preventDefault();
-    authService.updatePassword(this.state.currentPassword, this.state.newPassword)
+    authService
+      .updatePassword(this.state.currentPassword, this.state.newPassword)
       .then(() => {
         this.setState({
-          msg: 'Mot de passe modifié avec succès',
-          currentPassword: '',
-          newPassword: ''
+          msg: "Mot de passe modifié avec succès",
+          currentPassword: "",
+          newPassword: "",
         });
       })
-      .catch(err => this.setState({ msg: err.response.data.message }))
-  }
+      .catch((err) => this.setState({ msg: err.response.data.message }));
+  };
 
   deleteAccount = (event) => {
-    authService.deleteAccount()
-      .then(() => {
-        this.props.updateUser(false);
-      });
-  }
+    authService.deleteAccount().then(() => {
+      this.props.updateUser(false);
+    });
+  };
 
   render() {
-    console.log('props profile:', this.props)
-    if (this.props.user === false) return <Redirect to="/login" />
+    console.log("props profile:", this.props);
+    if (this.props.user === false) return <Redirect to="/login" />;
 
     return (
-      <div>
-        <h1>Bonjour {this.props.user.firstName}</h1>
-        <button className="btn logout" onClick={(e) => this.logout(e)}>Logout</button>
-        <Order />
-        <div className='infoUser'>
-          <h2>Mes informations personnelles</h2>
+      <div className="dashboard">
+        <h2>Bonjour {this.props.user.firstName}</h2>
+        <button className="btn logout" onClick={(e) => this.logout(e)}>
+          Me déconnecter
+        </button>
+        <hr />
+        <div className="profilOrder">
+          <h3>Mes achats</h3>
+          <Order />
+        </div>
+        <hr />
+        <div className="infoUser">
+          <h3>Mes informations personnelles</h3>
           <form onSubmit={this.handleSubmit}>
             {/* <p>
               <label>
@@ -118,88 +125,136 @@ class Profile extends React.Component {
                 <input type="text" name="civility" value={this.state.civility} onChange={this.handleChange} />
               </label>
             </p> */}
-            <p>
-              <label>
-                <em>Prénom</em>
-                <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                <em>Nom</em>
-                <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                <em>Email</em>
-                <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
-              </label>
-            </p>
-            {/* <p>
-              <label>
-                <em>Téléphone</em>
-                <input type="tel" name="telephone" value={this.state.telephone} onChange={this.handleChange} />
-              </label>
-            </p> */}
 
+            <label>
+              <em>Prénom</em>
+              <input
+                type="text"
+                name="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label>
+              <em>Nom</em>
+              <input
+                type="text"
+                name="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label>
+              <em>Email</em>
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label>
+              <em>Téléphone</em>
+              <input
+                type="tel"
+                name="telephone"
+                value={this.state.telephone}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            {/* 
             <PhoneInput
               country={'fr'}
               onlyCountries={['fr']}
               value={this.state.telephone}
               onChange={telephone => this.setState({ telephone })}
               placeholder="+33 6 12 34 56 78"
-            />
+            /> */}
 
-            <p>
-              <label>
-                <em>Adresse</em>
-                <input type="text" name="street" value={this.state.street} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                <em>Complément d'adresse</em>
-                <input type="text" name="supp" value={this.state.supp} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                <em>Code postal</em>
-                <input type="text" name="zip" value={this.state.zip} onChange={this.handleChange} />
-              </label>
-            </p>
-            <p>
-              <label>
-                <em>Ville</em>
-                <input type="text" name="city" value={this.state.city} onChange={this.handleChange} />
-              </label>
-            </p>
-            <button className="btn">Editer mes infos perso</button>
-          </form>
-
-          <h2>Modifier mon mot de passe</h2>
-          <form onSubmit={this.handleSubmitUpdatePassword} className='signup'>
-            <label>Mot de passe actuel:
-              <input type='password' name="currentPassword" value={this.state.currentPassword} onChange={e => this.handleChange(e)} />
+            <label>
+              <em>Adresse</em>
+              <input
+                type="text"
+                name="street"
+                value={this.state.street}
+                onChange={this.handleChange}
+              />
             </label>
 
-            <label>Nouveau mot de passe:
-              <input type='password' name="newPassword" value={this.state.newPassword} onChange={e => this.handleChange(e)} />
+            <label>
+              <em>Complément d'adresse</em>
+              <input
+                type="text"
+                name="supp"
+                value={this.state.supp}
+                onChange={this.handleChange}
+              />
             </label>
 
-            <button className="btn">Modifier mon mot de passe</button>
+            <label>
+              <em>Code postal</em>
+              <input
+                type="text"
+                name="zip"
+                value={this.state.zip}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label>
+              <em>Ville</em>
+              <input
+                type="text"
+                name="city"
+                value={this.state.city}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <button className="btn btnedit">Editer mes infos perso</button>
           </form>
-
-          {this.state.msg && (
-            <p className="msg">{this.state.msg}</p>
-          )}
-
-          <button className="btn" onClick={e => this.deleteAccount(e)}>Supprimer mon compte</button>
-
         </div>
+        <hr />
+        <div className="editpass">
+          <h3>Modifier mon mot de passe</h3>
+          <form onSubmit={this.handleSubmitUpdatePassword} className="signup">
+            <label>
+              Mot de passe actuel:
+              <input
+                type="password"
+                name="currentPassword"
+                value={this.state.currentPassword}
+                onChange={(e) => this.handleChange(e)}
+              />
+            </label>
+
+            <label>
+              Nouveau mot de passe:
+              <input
+                type="password"
+                name="newPassword"
+                value={this.state.newPassword}
+                onChange={(e) => this.handleChange(e)}
+              />
+            </label>
+
+            <button className="btn btneditpass">Modifier mon mot de passe</button>
+          </form>
+          
+          {this.state.msg && <p className="msg">{this.state.msg}</p>}
+          
+          
+        </div>
+        <hr/>
+        <button className="btn btndel" onClick={(e) => this.deleteAccount(e)}>
+          Supprimer mon compte
+        </button>
       </div>
-    )
+    );
   }
 }
 
--- alcove-react/src/components/products/DetailsProduct.js
+++ alcove-react/src/components/products/DetailsProduct.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import productsService from './products-service';
-import cartService from '../cart/cart-service';
-import "./DetailsProduct.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import productsService from "./products-service";
+import cartService from "../cart/cart-service";
+import "./DetailsProduct.css";
 
 class DetailsProduct extends React.Component {
   state = {
@@ -13,128 +13,165 @@ class DetailsProduct extends React.Component {
     ingredients: "",
     description: "",
     imageUrl: "",
-    quantity: 1
-  }
+    quantity: 1,
+  };
 
   handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
-  }
+  };
 
   handleChangeProductQuantity = (event) => {
     let regEx = /^[0-9\b]+$/; //autorise chiffre de 0 à 9
-    if (regEx.test(event.target.value) && Number(event.target.value) !== 0 && event.target.value.length < 3) {
+    if (
+      regEx.test(event.target.value) &&
+      Number(event.target.value) !== 0 &&
+      event.target.value.length < 3
+    ) {
       this.setState({ quantity: Number(event.target.value) });
     }
-  }
+  };
 
   decreaseQty = (event) => {
     let qty = Number(this.state.quantity) - 1;
     this.setState({ quantity: qty });
-  }
+  };
 
   increaseQty = (event) => {
     let qty = Number(this.state.quantity) + 1;
     this.setState({ quantity: qty });
-  }
+  };
 
   addToCart = (event, id) => {
     event.preventDefault();
 
     if (this.isAlreadyInCart(id)) {
-      console.log(id, ' déjà dans le panier')
+      console.log(id, " déjà dans le panier");
       return;
     }
 
-    cartService.addToCart(id, this.state.quantity)
+    cartService
+      .addToCart(id, this.state.quantity)
       .then((response) => {
-        this.props.updateCart(response)
-        console.log('produit ajouté au panier')
-        this.props.history.push('/')      // Redirection vers home
+        this.props.updateCart(response);
+        console.log("produit ajouté au panier");
+        this.props.history.push("/"); // Redirection vers home
       })
-      .catch(err => console.log('error delete product:', err))
-  }
+      .catch((err) => console.log("error delete product:", err));
+  };
 
   componentDidMount() {
-    productsService.getProduct(this.props.match.params.id)
-      .then(response => this.setState(response))
-      .catch(err => {
-        console.log(err)
+    productsService
+      .getProduct(this.props.match.params.id)
+      .then((response) => this.setState(response))
+      .catch((err) => {
+        console.log(err);
       });
   }
 
   handleFocus = (event) => {
     event.target.select();
-  }
+  };
 
   // Fonctions utilitaires
   isAlreadyInCart(id) {
     return this.props.cart
-      .map(item => item.product._id)        //Retourne un tableau d'id
-      .filter(propsId => propsId === id)    //Retourne un tableau avec l'id passée en paramètre
-      .length > 0 ? true : false;           //On n'ajoute rien dans le panier
+      .map((item) => item.product._id) //Retourne un tableau d'id
+      .filter((propsId) => propsId === id).length > 0 //Retourne un tableau avec l'id passée en paramètre
+      ? true
+      : false; //On n'ajoute rien dans le panier
   }
 
   render() {
     return (
-      <div className='detailsPdt'>
+      <div className="detailsPdt">
         <h3>{this.state.name}</h3>
-        <img src={this.state.imageUrl || "https://via.placeholder.com/375x250"} alt="product_pict" />
-        
-        <div className='price'>
+        <img
+          src={this.state.imageUrl || "https://via.placeholder.com/375x250"}
+          alt="product_pict"
+        />
+
+        <div className="price">
           <span>{this.state.unitPrice * this.state.quantity} €</span>
           {/* {Affichage du btn en fn du role} */}
-          {this.props.user.role === "USER" && !this.isAlreadyInCart(this.props.match.params.id) ?
-            (<button
+          {this.props.user.role === "USER" &&
+          !this.isAlreadyInCart(this.props.match.params.id) ? (
+            <button
               className="btn"
-              onClick={e => this.addToCart(e, this.props.match.params.id)}>Ajouter au panier</button>):
-              (<p className='already'>Produit déjà dans le panier</p>)}
+              onClick={(e) => this.addToCart(e, this.props.match.params.id)}
+            >
+              Ajouter au panier
+            </button>
+          ) : (
+            <p className="already">Produit déjà dans le panier</p>
+          )}
 
-          {this.props.user.role === "ADMIN" &&
-            (<>
-              <Link to={`/edit-product/${this.props.match.params.id}`}>Modifier</Link>
+          {this.props.user.role === "ADMIN" && (
+            <>
+              <Link to={`/edit-product/${this.props.match.params.id}`}>
+                Modifier
+              </Link>
               <button
                 className="btn"
-                onClick={(e) => this.deleteProductFromDB(e, this.props.match.params.id)}>Supprimer</button>
-            </>)}
+                onClick={(e) =>
+                  this.deleteProductFromDB(e, this.props.match.params.id)
+                }
+              >
+                Supprimer
+              </button>
+            </>
+          )}
         </div>
         {/* {Affichage quantité seulement pour USER si pas déja dans le panier} */}
-        {this.props.user.role !== "ADMIN" && !this.isAlreadyInCart(this.props.match.params.id) && (
-          <div className='qty'>
-            <label>Quantitée</label>
-            <div className='qtyBox'>
-            <button
-              className="btn btnup"
-              disabled={(this.state.quantity === 1 || this.state.quantity === 0) && true}
-              onClick={e => this.decreaseQty(e)}>-</button>
-            <input
-              type="text"
-              name="quantity"
-              value={this.state.quantity}
-              onFocus={this.handleFocus}
-              onChange={e => this.handleChangeProductQuantity(e)} />
-            <button
-              className="btn btndown"
-              disabled={this.state.quantity === 99 && true}
-              onClick={e => this.increaseQty(e)}>+</button>
+        {this.props.user.role !== "ADMIN" &&
+          !this.isAlreadyInCart(this.props.match.params.id) && (
+            <div className="qty">
+              <label>Quantitée</label>
+              <div className="qtyBox">
+                <button
+                  className="btn btnup"
+                  disabled={
+                    (this.state.quantity === 1 || this.state.quantity === 0) &&
+                    true
+                  }
+                  onClick={(e) => this.decreaseQty(e)}
+                >
+                  -
+                </button>
+                <input
+                  type="text"
+                  name="quantity"
+                  value={this.state.quantity}
+                  onFocus={this.handleFocus}
+                  onChange={(e) => this.handleChangeProductQuantity(e)}
+                />
+                <button
+                  className="btn btndown"
+                  disabled={this.state.quantity === 99 && true}
+                  onClick={(e) => this.increaseQty(e)}
+                >
+                  +
+                </button>
+              </div>
             </div>
-          </div>
-        )}
+          )}
         <Link to="/">Retour aux produits</Link>
         <div className="info-container">
-          <h2>Description</h2>
-          <p>{this.state.description}</p>
-        </div>
-        <div className="info-container">
-          <h2>Conseil d'utilisation</h2>
-          <p>{this.state.advice}</p>
-        </div>
-        <div className="info-container">
-          <h2>Ingrédients</h2>
-          <p>{this.state.ingredients}</p>
+          <section>
+            <h2>Description</h2>
+            <p>{this.state.description}</p>
+          </section>
+          <section>
+            <h2>Conseil d'utilisation</h2>
+            <p>{this.state.advice}</p>
+          </section>
+          <section>
+            <h2>Ingrédients</h2>
+            <p>{this.state.ingredients}</p>
+          </section>
         </div>
       </div>
-    )
+    );
   }
 }
 
